9cf0f20e6103efc6425125ed81b221fb329d168e
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2025-03-17 23:01:58 +0530

    added case-flipper

---
+++ case-flipper.js
@@ -0,0 +1,41 @@
+const inputFile = await Deno.open("input.txt", { read: true });
+const outputFile = await Deno.open("output.txt", { write: true });
+
+const readableStream = inputFile.readable;
+
+const decoderStream = new TextDecoderStream();
+const encoderStream = new TextEncoderStream();
+
+const letterTransform = (word) => {
+  let transformedWord = "";
+
+  for (const char of word) {
+    if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) {
+      transformedWord += String.fromCharCode(char.charCodeAt() + 32);
+    }
+
+    if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
+      transformedWord += String.fromCharCode(char.charCodeAt() - 32);
+    }
+
+    else if (char === " "){
+      transformedWord += char;
+    }
+  }
+
+  return transformedWord;
+};
+
+const caseTransform = new TransformStream({
+  transform(chunk, controller) {
+    const chunkData = chunk.split("\n").map(letterTransform).join("\n");
+
+    controller.enqueue(chunkData);
+  },
+});
+
+await readableStream
+  .pipeThrough(decoderStream)
+  .pipeThrough(caseTransform)
+  .pipeThrough(encoderStream)
+  .pipeTo(outputFile.writable);
